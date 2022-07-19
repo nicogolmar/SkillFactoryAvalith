12efd9167558cea6329868f2d5a70fd525712f9c
nicogolmar <nico.bocakapo09@gmail.com>
2022-07-19 12:49:36 -0300

    FirstActivityJs

---
+++ variables.js
@@ -0,0 +1,18 @@
+const name="Nicolas Golmar";
+const age = 26;
+const isMarried=false;
+const numArray=[1,2,3,4,5,6,7,8,9,10];
+const nameArray=["Franco" , "Debora", "Mariano" , "Gonzalo", "Matias"];
+
+const Persona = {
+    name:"Nicolas Golmar",
+    age : 26,
+    isMarried:true
+}
+
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(numArray);
+console.log(nameArray);
+console.log(Persona);
